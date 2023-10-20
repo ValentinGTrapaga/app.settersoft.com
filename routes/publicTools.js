ee70b64210b4f3e09fab77b880de333e7f23d7d3
Agustin Maillet <agustinmaillet47@gmail.com>
2023-10-20 13:28:00 -0300

    Added /es and /en routes

--- routes/publicTools.js
+++ routes/publicTools.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-// Ruta del VSL con calculadora
-router.get('/calc', async (req, res) => {
+// Ruta del VSL con calculadora (ENGLISH)
+router.get('/en/calc', async (req, res) => {
     // Get currency from query string (default: $)
     const currency = req.query.currency ? req.query.currency : '$';
-    res.render('public_tools/vsl_calc', { currency })
+    res.render('public_tools/vsl_calc_en', { currency })
 });
+
+// Ruta del VSL con calculadora (ESPAÑOL)
+router.get('/es/calc', async (req, res) => {
+    // Get currency from query string (default: $)
+    const currency = req.query.currency ? req.query.currency : '$';
+    res.render('public_tools/vsl_calc_es', { currency })
+});
+
+
+
 
 
 
