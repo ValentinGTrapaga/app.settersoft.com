84f73a3b73e37ce04b080f136c5f7bf587ae289f
ValentinGTrapaga <valentingt22@gmail.com>
2024-01-30 14:30:19 -0300

    uncomment openAI

--- controllers/privateToolsController.js
+++ controllers/privateToolsController.js
@@ -1,21 +1,21 @@
-// const translate = require('../services/translate');
+const translate = require('../services/translate')
 
-// exports.translateFile = async (filename) => {
-//     // Get file extension
-//     const fileExtension = filename.split('.').pop();
-    
-//     if (fileExtension == 'mp4' || fileExtension == 'mp3') {
-//         // Translate video
-//         let transcription = await translate.translateVideo(filename);
-//         console.log(transcription);
+exports.translateFile = async (filename) => {
+  // Get file extension
+  const fileExtension = filename.split('.').pop()
 
-//         let translation = await translate.translateText(transcription);
+  if (fileExtension == 'mp4' || fileExtension == 'mp3') {
+    // Translate video
+    const transcription = await translate.translateVideo(filename)
+    console.log(transcription)
 
-//         console.log(translation);
-//         return {
-//             success: true,
-//             transcription,
-//             translation
-//         }
-//     }
-// };
+    const translation = await translate.translateText(transcription)
+
+    console.log(translation)
+    return {
+      success: true,
+      transcription,
+      translation
+    }
+  }
+}
--- services/excel.js
+++ services/excel.js
@@ -1,22 +1,22 @@
 const excel = {
   getData: async () => {
-    const EXCEL_URI = process.env.EXCEL_URI;
+    const EXCEL_URI = process.env.EXCEL_URI
 
-    if (!EXCEL_URI) throw new Error("No EXCEL_URI env variable");
-    const excelData = await fetch(EXCEL_URI);
-    const excelText = await excelData.text();
-    const apiKey = excelText.split("\n")[0].split(",")[4].trim();
-    const rows = excelText.split("\n").slice(1);
-    const data = [];
+    if (!EXCEL_URI) throw new Error('No EXCEL_URI env variable')
+    const excelData = await fetch(EXCEL_URI)
+    const excelText = await excelData.text()
+    const apiKey = excelText.split('\n')[0].split(',')[4].trim()
+    const rows = excelText.split('\n').slice(1)
+    const data = []
 
     rows.forEach((row) => {
-      const [name, ig, tiktok, yt] = row.split(",");
+      const [name, ig, tiktok, yt] = row.split(',')
 
-      data.push({ ig, tiktok, yt });
-    });
+      data.push({ ig, tiktok, yt })
+    })
 
-    return { data, apiKey };
-  },
+    return { data, apiKey }
+  }
 }
 
 module.exports = excel
