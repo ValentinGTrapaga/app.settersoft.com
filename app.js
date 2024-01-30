fda4a201b879791693278db0a8960681a14cfcb6
ValentinGTrapaga <valentingt22@gmail.com>
2024-01-30 07:28:37 -0300

    add dev environment and scripts

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
-// require('./db');
+require('dotenv').config({
+    path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env'
+});
+require('./db');
 
 // Set view engine
 app.set('view engine', 'ejs');
@@ -24,5 +26,6 @@ app.use('/userFollowersData/', require('./routes/userFollowersData'));
 
 // Start server
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+    console.log('Server started on port 3000, process.env.MONGO_URI: ', process.env.MONGODB_URI)
+    console.log({ env: process.env.NODE_ENV })
 });
