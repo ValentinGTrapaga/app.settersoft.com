02acdcd8065382def3967ae355110d918e8de6ce
ValentinGTrapaga <valentingt22@gmail.com>
2024-01-30 14:29:09 -0300

    add eslint config

--- utils/getGrowthByDay.js
+++ utils/getGrowthByDay.js
@@ -13,12 +13,15 @@ function getGrowthByDay (itemArray) {
   for (const prop in group) {
     for (let i = 0; i < group[prop].length; i++) {
       if (i === 0) {
-        group[prop][i]['ig_growth'] = 0
+        group[prop][i].ig_growth = 0
+        group[prop][i].tiktok_growth = 0
       } else {
         const curr = group[prop][i]
         const prev = group[prop][i - 1]
-        const growth = curr.ig_followersCount - prev.ig_followersCount
-        group[prop][i]['ig_growth'] = growth
+        const ig_growth = curr.ig_followersCount - prev.ig_followersCount
+        const tiktok_growth = curr.tiktok_followersCount - prev.tiktok_followersCount
+        group[prop][i].ig_growth = ig_growth
+        group[prop][i].tiktok_growth = tiktok_growth || 0
       }
       flatArray.push(group[prop][i])
     }
