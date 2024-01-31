df2d3ecb94ae173e3d3e62aa43b7692126a37037
RamunnoAJ <ramunnoagustin@gmail.com>
2024-01-31 11:25:52 -0300

    feat: create new model page

--- routes/publicTools.js
+++ routes/publicTools.js
@@ -1,133 +1,157 @@
-const express = require('express');
-const multer = require('multer');
-const router = express.Router();
-const { generatePromise, generateScript } = require('../utils/openai.js');
-const { extractTextFromDocx } = require('../utils/extractTextFromDocx.js');
+const express = require('express')
+const multer = require('multer')
+const router = express.Router()
+const { generatePromise, generateScript } = require('../utils/openai.js')
+const { extractTextFromDocx } = require('../utils/extractTextFromDocx.js')
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
-    cb(null, "uploads/");
+    cb(null, 'uploads/')
   },
   filename: (_, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
+    cb(null, file.originalname)
+  }
+})
 
-const upload = multer({ storage });
+const upload = multer({ storage })
 
 // Ruta del VSL con calculadora (ENGLISH)
 router.get('/en/calc', async (req, res) => {
-    // Get currency from query string (default: $)
-    const currency = req.query.currency ? req.query.currency : '$';
-    res.render('public_tools/vsl_calc_en', { currency })
-});
+  // Get currency from query string (default: $)
+  const currency = req.query.currency ? req.query.currency : '$'
+  res.render('public_tools/vsl_calc_en', { currency })
+})
 
 // Ruta del VSL con calculadora (ESPAÑOL)
 router.get('/es/calc', async (req, res) => {
-    // Get currency from query string (default: $)
-    const currency = req.query.currency ? req.query.currency : '$';
-    res.render('public_tools/vsl_calc_es', { currency })
-});
-
-router.get('/es/organic-calc', async (req, res) => {
-    // Get currency from query string (default: $)
-    const currency = req.query.currency ? req.query.currency : '$';
-    res.render('public_tools/vsl_calc_organic_es', {currency})
-});
-
-router.get('/es/new-calc', async (req, res) => {
-    // Get currency from query string (default: $)
-    const currency = req.query.currency ? req.query.currency : '$';
-    res.render('public_tools/vsl_new_es', {currency})
-});
-
-router.get('/en/new-calc', async (req, res) => {
-    // Get currency from query string (default: $)
-    const currency = req.query.currency ? req.query.currency : '$';
-    res.render('public_tools/vsl_new_en', {currency})
-});
-
-router.get('/generate-promise', async (req, res) => {
-    res.render('public_tools/vsl_generate_promise')
+  // Get currency from query string (default: $)
+  const currency = req.query.currency ? req.query.currency : '$'
+  res.render('public_tools/vsl_calc_es', { currency })
 })
 
-router.post('/generate-promise/upload', upload.array("image"), async (req, res) => {
-    const files = req.files;
+router.get('/es/organic-calc', async (req, res) => {
+  // Get currency from query string (default: $)
+  const currency = req.query.currency ? req.query.currency : '$'
+  res.render('public_tools/vsl_calc_organic_es', { currency })
+})
+
+router.get('/es/new-calc', async (req, res) => {
+  // Get currency from query string (default: $)
+  const currency = req.query.currency ? req.query.currency : '$'
+  res.render('public_tools/vsl_new_es', { currency })
+})
+
+router.get('/en/new-calc', async (req, res) => {
+  // Get currency from query string (default: $)
+  const currency = req.query.currency ? req.query.currency : '$'
+  res.render('public_tools/vsl_new_en', { currency })
+})
+
+router.get('/generate-promise', async (req, res) => {
+  res.render('public_tools/vsl_generate_promise')
+})
+
+router.post(
+  '/generate-promise/upload',
+  upload.array('image'),
+  async (req, res) => {
+    const files = req.files
     try {
-        /** @type {{avatar: string, client: string, offer: string}} */
-        const conversations = { avatar: "", client: "", offer: "" };
+      /** @type {{avatar: string, client: string, offer: string}} */
+      const conversations = { avatar: '', client: '', offer: '' }
 
-        for await (const file of files) {
-            try {
-                const extractedText = await extractTextFromDocx(file.path);
-                const cleanedText = extractedText.replace(/\s+/g, " ");
+      for await (const file of files) {
+        try {
+          const extractedText = await extractTextFromDocx(file.path)
+          const cleanedText = extractedText.replace(/\s+/g, ' ')
 
-                if (file.path.includes("Avatar")) {
-                    conversations.avatar = cleanedText;
-                } else if (file.path.includes("cliente")) {
-                    conversations.client = cleanedText;
-                } else {
-                    conversations.offer = cleanedText;
-                }
-            } catch (error) {
-                console.error("Error processing file:", error);
-            }
+          if (file.path.includes('Avatar')) {
+            conversations.avatar = cleanedText
+          } else if (file.path.includes('cliente')) {
+            conversations.client = cleanedText
+          } else {
+            conversations.offer = cleanedText
+          }
+        } catch (error) {
+          console.error('Error processing file:', error)
         }
+      }
 
-        const completions = await generatePromise(conversations);
+      const completions = await generatePromise(conversations)
 
-        res.json({
-            message: "Files processed successfully",
-            completions,
-            error: null,
-        });
+      res.json({
+        message: 'Files processed successfully',
+        completions,
+        error: null
+      })
     } catch (error) {
-        console.error("Error processing files:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+      console.error('Error processing files:', error)
+      res.status(500).json({ error: 'Internal Server Error' })
     }
-});
+  }
+)
 
 router.get('/generate-script', async (req, res) => {
-    res.render('public_tools/vsl_generate_script');
-});
+  res.render('public_tools/vsl_generate_script')
+})
 
-router.post('/generate-script/upload', upload.array("script"), async (req, res) => {
-    const files = req.files;
+router.post(
+  '/generate-script/upload',
+  upload.array('script'),
+  async (req, res) => {
+    const files = req.files
     try {
-        /** @type {{avatar: string, client: string, offer: string, structure: string}} */
-        const conversations = { avatar: "", client: "", offer: "", structure: "" };
+      /** @type {{avatar: string, client: string, offer: string, structure: string}} */
+      const conversations = {
+        avatar: '',
+        client: '',
+        offer: '',
+        structure: ''
+      }
 
-        for await (const file of files) {
-            try {
-                const extractedText = await extractTextFromDocx(file.path);
-                const cleanedText = extractedText.replace(/\s+/g, " ");
+      for await (const file of files) {
+        try {
+          const extractedText = await extractTextFromDocx(file.path)
+          const cleanedText = extractedText.replace(/\s+/g, ' ')
 
-                if (file.path.includes("Avatar")) {
-                    conversations.avatar = cleanedText;
-                } else if (file.path.includes("cliente")) {
-                    conversations.client = cleanedText;
-                } else if (file.path.includes("oferta")) {
-                    conversations.offer = cleanedText;
-                } else {
-                    conversations.structure = cleanedText;
-                }
-            } catch (error) {
-                console.error("Error processing file:", error);
-            }
+          if (file.path.includes('Avatar')) {
+            conversations.avatar = cleanedText
+          } else if (file.path.includes('cliente')) {
+            conversations.client = cleanedText
+          } else if (file.path.includes('oferta')) {
+            conversations.offer = cleanedText
+          } else {
+            conversations.structure = cleanedText
+          }
+        } catch (error) {
+          console.error('Error processing file:', error)
         }
+      }
 
-        const completions = await generateScript(conversations);
+      const completions = await generateScript(conversations)
 
-        res.json({
-            message: "Files processed successfully",
-            completions,
-            error: null,
-        });
+      res.json({
+        message: 'Files processed successfully',
+        completions,
+        error: null
+      })
     } catch (error) {
-        console.error("Error processing files:", error);
-        res.status(500).json({ error: "Internal Server Error" });
+      console.error('Error processing files:', error)
+      res.status(500).json({ error: 'Internal Server Error' })
     }
+  }
+)
 
-});
+router.get('/en/upload-model', async (req, res) => {
+  res.render('public_tools/vsl_upload_model_en')
+})
 
-module.exports = router;
+router.get('/es/upload-model', async (req, res) => {
+  res.render('public_tools/vsl_upload_model_es')
+})
+
+router.post('/upload-model', async (req, res) => {
+  res.send(req.body)
+})
+
+module.exports = router
